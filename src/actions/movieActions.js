c74320496a80bed6fc7be0ac30f069451ff337fe
Ryan Ellis <ryan.ellis33@gmail.com>
2021-04-03 05:31:25 -0600

    Fresh Commit...Everything is working as of this commit

---
+++ src/actions/movieActions.js
@@ -0,0 +1,75 @@
+import actionTypes from '../constants/actionTypes';
+import runtimeEnv from '@mars/heroku-js-runtime-env'
+
+
+function moviesFetched(movies) {
+    return {
+        type: actionTypes.FETCH_MOVIES,
+        movies: movies
+    }
+}
+
+function movieFetched(movie) {
+    return {
+        type: actionTypes.FETCH_MOVIE,
+        selectedMovie: movie
+    }
+}
+
+function movieSet(movie) {
+    return {
+        type: actionTypes.SET_MOVIE,
+        selectedMovie: movie
+    }
+}
+
+export function setMovie(movie) {
+    return dispatch => {
+        dispatch(movieSet(movie));
+    }
+}
+
+export function fetchMovie(movie_title) {
+    const env = runtimeEnv();
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/movies/${movie_title}?reviews=true`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            mode: 'cors'
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json()
+        }).then((res) => {
+            dispatch(movieFetched(res.movie));
+        }).catch((e) => console.log(e));
+    }
+}
+
+export function fetchMovies() {
+    const env = runtimeEnv();
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/movies?reviews=true`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            mode: 'cors'
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json()
+        }).then((res) => {
+            console.log(res.movie)
+            dispatch(moviesFetched(res.movie));
+        }).catch((e) => console.log(e));
+    }
+}
---
+++ src/components/movie.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import { fetchMovie } from "../actions/movieActions";
+import MovieDetail from "../components/moviedetail"
+
+// support routing
+
+function Movie(props) {
+    const [selectedMovie] = useState(props.selectedMovie);
+    const params = useParams();
+    const movie_title = params.movie_title;
+    console.log(movie_title);
+    const dispatch = useDispatch();
+    if (selectedMovie == null) {
+        dispatch(fetchMovie(movie_title));
+    }
+
+    return (<MovieDetail movieId={movie_title} />)
+}
+
+export default Movie;
